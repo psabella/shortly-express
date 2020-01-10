995a3afa5ae9c6a58eb35b34f488b005a5595160
psabella <pamsabella@gmail.com>
2020-01-10 12:55:11 -0800

    added url path for signup and login, working on making the tests pass

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const models = require('./models');
 const app = express();
 
 app.set('views', `${__dirname}/views`);
+// set the view engine to ejs
 app.set('view engine', 'ejs');
 app.use(partials());
 app.use(bodyParser.json());
@@ -16,68 +17,110 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
 
+// use res.render to load up an ejs view file
 
-app.get('/', 
-(req, res) => {
-  res.render('index');
-});
+// this will give us all links page
+app.get('/',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/create', 
-(req, res) => {
-  res.render('index');
-});
+// this will give us shorten page
+app.get('/create',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/links', 
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
-
-app.post('/links', 
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
-    // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
-
-  return models.Links.get({ url })
-    .then(link => {
-      if (link) {
-        throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
+// will give us a page with arrays consisting objects, each object contains id and url, baseUrl, code for shortened url, title, and visits
+app.get('/links',
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
       });
-    })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
-    });
-});
+  });
+
+app.post('/links',
+  (req, res, next) => {
+    var url = req.body.url;
+    if (!models.Links.isValidUrl(url)) {
+      // send back a 404 if link is not valid
+      return res.sendStatus(404);
+    }
+
+    return models.Links.get({ url })
+      .then(link => {
+        if (link) {
+          throw link;
+        }
+        return models.Links.getUrlTitle(url);
+      })
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
+        throw link;
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
+      });
+  });
 
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
+app.get('/login',
+  (req, res) => {
+    res.render('login');
+  });
 
+app.get('/signup',
+  (req, res) => {
+    res.render('signup');
+  });
+
+app.post('/signup',
+  (req, res, next) => {
+    console.log(req.body);
+    var username = req.body.username;
+    var password = req.body.password;
+
+    return models.Users.create({
+      username: username,
+      password: password
+    })
+      .then((user) => {
+        return user;
+
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(user => {
+        res.status(200).send(user);
+      });
+
+
+
+
+
+
+
+  }
+)
 
 
 /************************************************************/
